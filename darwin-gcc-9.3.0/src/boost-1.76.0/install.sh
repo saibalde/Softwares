c554f4cc47cba84e98b7e2e0cf9937497bed5e94
Saibal De <saibal.de.bnk@gmail.com>
2021-05-19 14:44:21 -0400

    Update Boost configuration & install script

--- darwin-gcc-9.3.0/src/boost-1.76.0/install.sh
+++ darwin-gcc-9.3.0/src/boost-1.76.0/install.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-cd ./tools/build
-./bootstrap.sh
-./b2 install --prefix=$HOME/Softwares/darwin-gcc-9.3.0
-cd -
+#   cd ./tools/build
+#   ./bootstrap.sh
+#   ./b2 install --prefix=$HOME/Softwares/darwin-gcc-9.3.0
+#   cd -
 
 $HOME/Softwares/darwin-gcc-9.3.0/bin/b2 \
     toolset=gcc-9 \
     variant=release \
+    link=static \
     --prefix=$HOME/Softwares/darwin-gcc-9.3.0 \
     install
